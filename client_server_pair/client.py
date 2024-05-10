e4e61740140b75381d63ec78b1264186f8a87168
rhit-brelanre <brelanre@rose-hulman.edu>
2024-05-10 14:38:46 -0400

    set up helper functions

--- client_server_pair/client.py
+++ client_server_pair/client.py
@@ -1,62 +1,88 @@
 import sys
 import socket
 import rsa
+import subprocess
+
+BUFFER_SIZE = 4096
 
 def authenticate_server(client_socket, public_key):
 
-    send_resp(client_socket, public_key)
+    send_resp(client_socket, str(public_key).encode())
 
-    encrypt_key = recv_msg(client_socket)
+    encrypt_key = recv_msg(client_socket).decode()
 
     return encrypt_key
 
-def send_resp(client_socket, message):
-    pass
+def encrypt(msg, key):
+    encrypted_message = rsa.encrypt(msg.encode(), key)
+    return encrypted_message
+
+def decrypt(msg, key):
+    decrypted_message = rsa.decrypt(msg, key).decode()
+    return decrypted_message
+
+
+def send_resp(client_socket, msg):
+    client_socket.sendall(msg)
 
 def recv_msg(client_socket):
-    pass
+    response = b''
+    while True:
+        data = client_socket.recv(BUFFER_SIZE)
+        if not data:
+            break
+        response += data
 
+    return response
+
+def execute_command(command):
+    command_arr = split_ignore_quotes(command, ' ')
+    command_resp = subprocess.check_output(command_arr)
+    return command_resp
+
+def split_ignore_quotes(string, sep):
+    if string.find('"') == -1:
+        return string
+    else:
+        split_str = string.split('"')
+        complete_split = []
+        i = 1
+        for substr in split_str:
+            if i%2 == 1:
+                complete_split += substr.split(' ')
+            else:
+                complete_split += '\"' + substr + '\"'
+    return complete_split
 
 
 def client_program():
     if(len(sys.argv) != 3):
         print("Usage: python client.py <server IP address> <server port number>")
         sys.exit()
-    port = sys.argv[2]
+
+    port = int(sys.argv[2])
     server_ip = socket.gethostbyname(sys.argv[1])
 
     server_addr = (server_ip, port)
 
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # instantiate
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect(server_addr)
 
-    public_key, private_key = RSA.newkeys(512)
+    public_key, private_key = rsa.newkeys(512)
 
     encrypt_key = authenticate_server(client_socket, public_key)
 
     while True:
         encrypted_command = recv_msg(client_socket)
-        decryped_command = decrypt(encrypted_command, )
+        decrypted_command = decrypt(encrypted_command, private_key)
+        if decrypted_command == 'exit':
+            break
 
-
+        response = execute_command(decrypted_command)
+        encrypted_response = encrypt(response, encrypt_key)
+        send_resp(client_socket, encrypted_response)
+        
     client_socket.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 if __name__ == '__main__':
     client_program()
--- client_server_pair/server.py
+++ client_server_pair/server.py
@@ -4,98 +4,81 @@ import threading
 import subprocess
 
 import rsa
-from Crypto.Cipher import PKCS1_OAEP
-
-COMMAND = 1
-EXIT = -1
+import rsa.randnum
 
 
-# takes a connection, performs the communications necessary to authenticate, and returns the public key corresponding to that client
+BUFFER_SIZE = 4096
+
+
 def authenticate_client(conn, public_key):
-    # Receive the client's public key
-    encrypt_key = recv_msg(conn)
-
-    send_resp(conn, public_key)
-    # Perform authentication process here
-    # NOTE: For now, just accept any public key and return it
-
-    
-    # Return the public key
+    print("about to send keys")
+    encrypt_key = recv_msg(conn).decode()
+    print("got encryption key")
+    send_resp(conn, .encode())
+    print("sent encryption key")
     return encrypt_key
 
-# takes an encrypted message and a key and returns the decrypted message
 def decrypt(msg, key):
-    rsa_key = RSA.import_key(key)
-    cipher = PKCS1_OAEP.new(rsa_key)
-    decrypted_msg = cipher.decrypt(msg)
-    return decrypted_msg
+    decrypted_message = rsa.decrypt(msg, key).decode()
+    return decrypted_message
 
-# takes a message and a key and encrypts the message
 
-# NOTE: Uses RSA, we can change the algorithm later if needed
 def encrypt(msg, key):
-    rsa_key = RSA.import_key(key)
-    cipher = PKCS1_OAEP.new(rsa_key)
-    encrypted_msg = cipher.encrypt(msg)
-    return encrypted_msg    
+    encrypted_message = rsa.encrypt(msg.encode(), key)
+    return encrypted_message  
 
 
-# receives and returns a message from a connection
-def recv_msg(conn):
-    pass
-
-# sends a message to a connection
-def send_resp(conn, resp):
-    pass
-
-# splits a string on a separator but leaves portions inside quotes together (used for commands)
-def split_ignore_quotes(str, sep):
-    if str.find('"') == -1:
-        return str
-    else:
-        split_str = str.split('"')
-        complete_split = []
-        i = 1
-        for substr in split_str:
-            if i%2 == 1:
-                complete_split += substr.split(' ')
-            else:
-                complete_split += '\"' + substr + '\"'
-    return complete_split
-
-# executes a command and returns the output
-def execute_command(command):
-    command_arr = split_ignore_quotes(command, ' ')
-    command_resp = subprocess.check_output(command_arr)
-    return command_resp
-
-# thread to handle the server's actions
-def server_thread(client_socket):
-    public_key, private_key = RSA.newkeys(512)
-    encrypt_key = authenticate_client(client_socket, public_key)
+def recv_msg(client_socket):
+    response = b''
     while True:
-        msg = recv_msg(client_socket)
-        decrypted_msg = decrypt(msg, key)
-        #do stuff with the message then send a response - add stuff for anything else it needs to do
-        if decrypted_msg[0] == COMMAND:
-            resp = execute_command(decrypted_msg[1:])
-            encrypted_resp = encrypt(resp, private_key)
-            send_resp(client_socket, encrypted_resp)
-        elif decrypted_msg[0] == EXIT:
+        data = client_socket.recv(BUFFER_SIZE)
+        if not data:
             break
+        response += data
+
+    return response
+
+def send_resp(client_socket, msg):
+    client_socket.sendall(msg)
+
+def server_thread(client_socket):
+    print("in thread")
+    public_key, private_key = rsa.newkeys(512)
+    print("generated needed keys")
+    print("public key: ", str(public_key))
+    print("private key: ", str(private_key))
+    encrypt_key = authenticate_client(client_socket, public_key)
+    print("succesfully authenticated client")
+    while True:
+        command = input(" -> ")
+        encrypted_command = encrypt(command, encrypt_key)
+        send_resp(client_socket, encrypted_command)
+
+        if command == 'exit':
+            break
+
+        response = recv_msg(client_socket)
+        decrypted_response = decrypt(response, private_key)
+        decrypted_msg = decrypt(msg, key)
     client_socket.close()
 
-# main program to listen and start the threads
 def server_program():
-    port = 3000
+    if((len(sys.argv)) != 2):
+        print("Usage: python server.py <server port number>")
+        sys.exit()
+    
+    port = int(sys.argv[1])
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     server_socket.bind(('localhost', port))
 
     server_socket.listen(5)
 
     while True:
+        print("connected")
         client_socket, address = server_socket.accept()
         t = threading.Thread(target=server_thread, args=(client_socket,))
         t.start()
